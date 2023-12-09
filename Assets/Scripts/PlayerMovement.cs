c4c93a73a9b5f105cd4c0c01bff1be243095f9da
Timur-Tsvetkov <timurtcv@gmail.com>
2023-12-09 17:02:36 +0300

    add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float force = 65f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody player = GetComponent<Rigidbody>();
+        
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))    
+        {
+            player.AddForce(force * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+
+        }
+        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))    
+        {
+            player.AddForce(-force * Time.deltaTime, 0f, 0f, ForceMode.VelocityChange);
+
+        }
+    }
+}
