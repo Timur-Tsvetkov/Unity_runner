902c3400e76a8f2abc3c3da44a5333cd812a9f82
Timur-Tsvetkov <timurtcv@gmail.com>
2024-01-13 16:29:09 +0300

    Add ObstacleMovement and Generation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform player;
+    public PlayerMovement movement;
+
+    public GameObject obstaclePrefab;
+    public Transform obstacles;
+    public int obstacleStartX = 100;
+
+    private void Spawn()
+    {
+        int i;
+        for (i = -7; i < 7; i += 7)
+        {
+            Instantiate(obstaclePrefab, 
+                new Vector3(UnityEngine.Random.Range(i, i + 7), 1, obstacleStartX), Quaternion.identity, obstacles);
+        }
+    }
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.5f, 0.5f);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
